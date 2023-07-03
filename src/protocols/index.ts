8e635464bb5d6072966c89a5665803005dbaad05
Alicia T Nishiyama <nishiyamaeel@gmail.com>
2023-07-03 17:39:17 -0300

    Task types

--- src/protocols/index.ts
+++ src/protocols/index.ts
@@ -7,3 +7,33 @@ export type User = {
 
 export type CreateUser = Omit<User, "id">;
 
+export enum Area{
+    Eletrical = "Eletrical",
+    Mechanical = "Mechanical",
+    Naval = "Naval",
+    Eletronic = "Eletronic",
+    Marketing = "Marketing",
+    Management = "Management"
+}
+
+type Tool = {
+    name: string,
+    quantity: number;
+    description: string
+}
+
+export type Task = {
+    id: number,
+    user_id: number,
+    name: string,
+    task: string,
+    area: Area,
+    startsAt: Date,
+    endsAt?: Date,
+    createdAt: Date,
+    updatedAt: Date,
+    tools: Tool[]
+}
+
+export type CreateTask = Omit<Task, "id" | "createdAt" | "updatedAt">
+
